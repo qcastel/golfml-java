1f0b2bd818e71918510fd087495eb4bf181ed24e
Quentin CASTEL <mail@quentin-castel.fr>
2014-05-10 22:12:56 +0200

    Implements GPS point

--- GolfLM/src/com/golflm/models/GPSPoint.java
+++ GolfLM/src/com/golflm/models/GPSPoint.java
@@ -1,5 +1,54 @@
 package com.golflm.models;
 
+/**
+ * GPS point is an geographic coordinate system representation,
+ *  representing by a couple of two double (latitude, longitude)
+ * @author castel
+ *
+ */
 public class GPSPoint {
 
+    protected Double latitude;
+    protected Double longitude;
+    
+    /**
+     * GPS point in a geographic system
+     * @param latitude
+     * @param longitude
+     */
+    public GPSPoint(Double latitude, Double longitude) {
+        super();
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    /**
+     * @return the latitude
+     */
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * @return the longitude
+     */
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+    
+    
 }
