c188d112c91531dff5c6fdc3df7b0b92337f6e86
quentin <quentincastel86@gmail.com>
2014-05-26 23:08:15 +0100

    Implement Amenety

--- GolfLM/src/com/golflm/models/Amenety.java
+++ GolfLM/src/com/golflm/models/Amenety.java
@@ -2,10 +2,135 @@ package com.golflm.models;
 
 public class Amenety implements Comparable<Amenety>{
 
+	private final String content;
+	
+	private final AmenetyType type;
+	private final String other;
+	
+    /**
+     * Use this builder for creating an Address instance.
+     * Required parameters are in the constructor and optional have a dedicated method.
+     * 
+     * Example of use : 
+     * Address a = Address.Builder("France", "86000").region("Poitou-Charente).build();
+     * @author quentin
+     *
+     */
+    public static class Builder {
+    	
+    	private final String content;
+    	
+    	private AmenetyType type;
+    	private String other;
+    	
+        public Builder(String content){
+            if (content == null) {
+                throw new NullPointerException(
+                        "content is null");
+            }
+        	this.content = content;
+        }
+        
+        public Builder type(AmenetyType type) {
+        	this.type = type;
+        	return this;
+        }
+        
+        public Builder other(String other) {
+        	this.other = other;
+        	return this;
+        }
+
+        public Amenety build() {
+        	return new Amenety(this);
+        }
+    }
+    
+    private Amenety(Builder builder) {
+    	this.content = builder.content;
+    	this.type = builder.type;
+    	this.other = builder.other;
+    }
+    
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public AmenetyType getType() {
+		return type;
+	}
+
+	/**
+	 * @return the other
+	 */
+	public String getOther() {
+		return other;
+	}
+
 	@Override
 	public int compareTo(Amenety o) {
-		// TODO Auto-generated method stub
+		int order = this.content.compareTo(o.content);
+		if(order != 0) { return order;}
+		order = this.type.compareTo(o.type);
+		if(order != 0) { return order;}
+		order = this.other.compareTo(o.other);
+		if(order != 0) { return order;}
 		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((other == null) ? 0 : other.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Amenety other = (Amenety) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (this.other == null) {
+			if (other.other != null)
+				return false;
+		} else if (!this.other.equals(other.other))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Amenety [content=" + content + ", type=" + type + ", other="
+				+ other + "]";
 	}
 
 }
---
+++ GolfLM/src/com/golflm/models/AmenetyType.java
@@ -0,0 +1,5 @@
+package com.golflm.models;
+
+public enum AmenetyType {
+	PRACTICE, STORE, FOOD, CORPORATE, GOLFERS, BATHROOM, WATER, OTHER
+}
